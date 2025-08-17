17c7b88592a0dd1bb5fcaf993b7f41ee4c5b11cb
Dmitrii Lipin <dimich3d@ya.ru>
2025-08-17 16:34:47 +0200

    scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'config/button_config.dart';
 
 void main() {
@@ -21,36 +22,125 @@ class WearOSApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late PageController _pageController;
+  int _currentPage = 0;
+  late List<ButtonConfig> _buttons;
+  late int _totalPages;
+
+  @override
+  void initState() {
+    super.initState();
+    _buttons = AppConfig.getButtons();
+    _totalPages = (_buttons.length / 4).ceil();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _handleKeyEvent(RawKeyEvent event) {
+    if (event is RawKeyDownEvent) {
+      if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+        if (_currentPage < _totalPages - 1) {
+          _pageController.nextPage(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+        }
+      } else if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+        if (_currentPage > 0) {
+          _pageController.previousPage(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+        }
+      }
+    }
+  }
+
+  List<ButtonConfig> _getButtonsForPage(int pageIndex) {
+    final startIndex = pageIndex * 4;
+    final endIndex = (startIndex + 4).clamp(0, _buttons.length);
+    return _buttons.sublist(startIndex, endIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
     const double screenSize = 320.0;
-    final buttons = AppConfig.getButtons();
-    
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Container(
-          width: screenSize,
-          height: screenSize,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.grey[900],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(60.0),
-            child: GridView.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: 15,
-              crossAxisSpacing: 15,
-              children: buttons.map((config) => 
-                _buildButton(context, config)
-              ).toList(),
+
+    return RawKeyboardListener(
+      focusNode: FocusNode()..requestFocus(),
+      onKey: _handleKeyEvent,
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Container(
+            width: screenSize,
+            height: screenSize,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.grey[900],
+            ),
+            child: Stack(
+              children: [
+                PageView.builder(
+                  controller: _pageController,
+                  itemCount: _totalPages,
+                  onPageChanged: (page) {
+                    setState(() {
+                      _currentPage = page;
+                    });
+                  },
+                  itemBuilder: (context, pageIndex) {
+                    final pageButtons = _getButtonsForPage(pageIndex);
+                    return _buildButtonGrid(context, pageButtons);
+                  },
+                ),
+                if (_totalPages > 1)
+                  Positioned(
+                    bottom: 20,
+                    left: 0,
+                    right: 0,
+                    child: _buildPageIndicator(),
+                  ),
+              ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildButtonGrid(BuildContext context, List<ButtonConfig> buttons) {
+    final List<Widget> buttonWidgets = [];
+    
+    for (int i = 0; i < 4; i++) {
+      if (i < buttons.length) {
+        buttonWidgets.add(_buildButton(context, buttons[i]));
+      } else {
+        buttonWidgets.add(Container());
+      }
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(60.0),
+      child: GridView.count(
+        physics: const NeverScrollableScrollPhysics(),
+        crossAxisCount: 2,
+        mainAxisSpacing: 15,
+        crossAxisSpacing: 15,
+        children: buttonWidgets,
       ),
     );
   }
@@ -82,17 +172,37 @@ class MainScreen extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildPageIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        _totalPages,
+        (index) => Container(
+          margin: const EdgeInsets.symmetric(horizontal: 4),
+          width: 8,
+          height: 8,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: _currentPage == index
+                ? Colors.white
+                : Colors.white.withOpacity(0.3),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class ButtonScreen extends StatelessWidget {
   final ButtonConfig config;
-  
+
   const ButtonScreen({super.key, required this.config});
 
   @override
   Widget build(BuildContext context) {
     const double screenSize = 320.0;
-    
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
